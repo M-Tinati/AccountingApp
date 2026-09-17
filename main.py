3a00cd28532ef675f0b672a215169606d95269a7
M-Tinati <tinatigithub@gmail.com>
2026-09-17 12:52:17 +0330

    update

--- main.py
+++ main.py
@@ -1,86 +1,12 @@
-print("========== WORLD SIMULATOR ==========")
+import sys
 
-Temperature = 24.5
+from PySide6.QtWidgets import QApplication , QMainWindow
 
-#ورودی ها از کاربر
-World_Name = input("Enter world name: ")
-Population = int(input("Enter Population: "))
-World_Active = int(input("Is the game active? (1/0): "))
+app = QApplication(sys.argv)
 
-print()
-print("========== WORLD CREATED ==========")
-print("World:", World_Name)
-print("Temperature:", Temperature)
+windows = QMainWindow()
 
-#لیستی از موجودات
-Creatures = []
-
-
-if Population >= 3:
-
-    # ساخت موجودات فقط یک بار
-    for i in range(Population):
-
-        print()
-        print("Enter information for Creature", i + 1)
-
-        Creature_Name = input("Name: ")
-        Creature_Age = int(input("Age: "))
-        Creature_Health = int(input("Health: "))
-
-        x = int(input("Position X: "))
-        y = int(input("Position Y: "))
-
-        Creature = {
-            "name": Creature_Name,
-            "age": Creature_Age,
-            "health": Creature_Health,
-            "position": (x, y)
-        }
-        
-        Creatures.append(Creature)
-
-else:
-    print("Population must be at least 3.")
-
-day = 1
-
-while day < 5:
-
-    print()
-    print("========== DAY", day, "==========")
-    
-    new_creatures = Creatures.copy()    
-    # کم شدن Health موجودات
-    for creature in new_creatures:        
-        creature["health"] = creature["health"] - 10
-        if creature["health"] <= 0:
-            Creatures.remove(creature)
-            
-        print("Creature:", creature["name"])
-        print("Age:", creature["age"])
-        print("Health:", creature["health"])
-        print("Position:", creature["position"])
-        print()
-    
-    if World_Active == 1:
-        print("========== WORLD STATUS ==========")
-        print("World is running...")
-        print("The world can continue.")
-
-    elif World_Active == 0:
-        print("========== WORLD STATUS ==========")
-        print("World is stopped.")
-        print("Simulation cannot continue.")
-
-    else:
-        print("Invalid world status.")
-
-    day += 1
-
-print("============")
-
-
-
-
-
+windows.setWindowTitle("Factor")
+windows.resize(900,600)
+windows.show()
+sys.exit(app.exec())
--- train.py
+++ train.py
@@ -1,10 +1,54 @@
-def check_population(population):
-    if population > 0:
-        return "World has life"
-    elif population == 0:
-        return "World is empty"
-    elif  population < 0:
-        return "Invalid population"
+# Creatures = [
+#     {"name": "Wolf", "age": 7, "health": 80},
+#     {"name": "Bear", "age": 10, "health": 45},
+#     {"name": "Fox", "age": 3, "health": 20}
+# ]
+
+# def get_creature_status(creature):
+#     if creature["health"] >= 70:
+#         return "Healthy"
+#     elif creature["health"] >= 30:
+#         return "Weak"
+#     else:
+#         return "Critical"
+
+# for creature in Creatures:
+#     status = get_creature_status(creature)
+#     print(f"{creature['name']} -> {status}")
     
-status_population = check_population(10)
-print(status_population)
+    
+# try:
+#     age = int(input("Age: "))
+# except ValueError:
+#     print("Invalid age!")
+    
+# try:
+#     health = int(input("Health: "))
+# except ValueError:
+#     print("Invalid health.")
+# finally:
+#     print("Input process finished.") 
+
+# with open("world.txt","a") as file:
+#     file.write("\nRead")
+    
+# with open("world.txt", "r") as file:
+#     content = file.read()
+
+# print(content)
+
+import json
+
+# creatures = [
+#     {"name": "Wolf", "health": 80},
+#     {"name": "Bear", "health": 25},
+#     {"name": "Fox", "health": 60}
+# ]
+
+# with open("creatures.json", "w") as file:
+#     json.dump(creatures, file)  
+    
+with open("creatures.json", "r") as file:
+    creatures = json.load(file)
+
+print(creatures)
