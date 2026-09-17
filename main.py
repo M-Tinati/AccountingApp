6289ae16727a515408f8f98ed37052ea9c1071c4
M-Tinati <tinatigithub@gmail.com>
2026-09-17 17:46:03 +0330

    update

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 
-from PySide6.QtWidgets import QApplication , QMainWindow
+from PySide6.QtWidgets import QApplication , QMainWindow , QLabel , QPushButton
 
 app = QApplication(sys.argv)
 
@@ -8,5 +8,12 @@ windows = QMainWindow()
 
 windows.setWindowTitle("Factor")
 windows.resize(900,600)
+label = QLabel("Factor Accounting System",windows)
+label.move(50,50)
+buttom = QPushButton("ثبت محصول", windows)
+buttom.move(50,100)
+def pushbuttom():
+    print("کلیک شد")
+buttom.clicked.connect(pushbuttom)
 windows.show()
 sys.exit(app.exec())
