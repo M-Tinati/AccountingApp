11ed9b9c980e896f1b359a57d21b12e42d9caf09
M-Tinati <tinatigithub@gmail.com>
2026-09-17 18:01:06 +0330

    update

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 
-from PySide6.QtWidgets import QApplication , QMainWindow , QLabel , QPushButton
+from PySide6.QtWidgets import QApplication , QMainWindow , QLabel , QPushButton , QLineEdit
 
 app = QApplication(sys.argv)
 
@@ -12,8 +12,14 @@ label = QLabel("Factor Accounting System",windows)
 label.move(50,50)
 buttom = QPushButton("ثبت محصول", windows)
 buttom.move(50,100)
+InputUser = QLineEdit(windows)
+
+InputUser.move(200,50)
 def pushbuttom():
-    print("کلیک شد")
-buttom.clicked.connect(pushbuttom)
+    if InputUser.text() == "":
+        print("is not none define")
+    else:
+        print(InputUser.text())
+buttom.clicked.connect(pushbuttom)    
 windows.show()
 sys.exit(app.exec())
--- train.py
+++ train.py
@@ -1,52 +1,52 @@
-# Creatures = [
-#     {"name": "Wolf", "age": 7, "health": 80},
-#     {"name": "Bear", "age": 10, "health": 45},
-#     {"name": "Fox", "age": 3, "health": 20}
-# ]
+Creatures = [
+    {"name": "Wolf", "age": 7, "health": 80},
+    {"name": "Bear", "age": 10, "health": 45},
+    {"name": "Fox", "age": 3, "health": 20}
+]
 
-# def get_creature_status(creature):
-#     if creature["health"] >= 70:
-#         return "Healthy"
-#     elif creature["health"] >= 30:
-#         return "Weak"
-#     else:
-#         return "Critical"
+def get_creature_status(creature):
+    if creature["health"] >= 70:
+        return "Healthy"
+    elif creature["health"] >= 30:
+        return "Weak"
+    else:
+        return "Critical"
 
-# for creature in Creatures:
-#     status = get_creature_status(creature)
-#     print(f"{creature['name']} -> {status}")
+for creature in Creatures:
+    status = get_creature_status(creature)
+    print(f"{creature['name']} -> {status}")
     
     
-# try:
-#     age = int(input("Age: "))
-# except ValueError:
-#     print("Invalid age!")
+try:
+    age = int(input("Age: "))
+except ValueError:
+    print("Invalid age!")
     
-# try:
-#     health = int(input("Health: "))
-# except ValueError:
-#     print("Invalid health.")
-# finally:
-#     print("Input process finished.") 
+try:
+    health = int(input("Health: "))
+except ValueError:
+    print("Invalid health.")
+finally:
+    print("Input process finished.") 
 
-# with open("world.txt","a") as file:
-#     file.write("\nRead")
+with open("world.txt","a") as file:
+    file.write("\nRead")
     
-# with open("world.txt", "r") as file:
-#     content = file.read()
+with open("world.txt", "r") as file:
+    content = file.read()
 
-# print(content)
+print(content)
 
 import json
 
-# creatures = [
-#     {"name": "Wolf", "health": 80},
-#     {"name": "Bear", "health": 25},
-#     {"name": "Fox", "health": 60}
-# ]
+creatures = [
+    {"name": "Wolf", "health": 80},
+    {"name": "Bear", "health": 25},
+    {"name": "Fox", "health": 60}
+]
 
-# with open("creatures.json", "w") as file:
-#     json.dump(creatures, file)  
+with open("creatures.json", "w") as file:
+    json.dump(creatures, file)  
     
 with open("creatures.json", "r") as file:
     creatures = json.load(file)
