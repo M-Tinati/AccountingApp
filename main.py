fd82388cfe1d5b465dd5debb8a41ff0ef3aa33b2
M-Tinati <tinatigithub@gmail.com>
2026-09-17 18:50:41 +0330

    update

--- main.py
+++ main.py
@@ -1,37 +1,38 @@
 import sys
 
-from PySide6.QtWidgets import QApplication , QMainWindow , QLabel , QPushButton , QLineEdit
+from PySide6.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    QLabel,
+    QPushButton,
+    QLineEdit,
+    QWidget,
+    QFormLayout
+)
 
 app = QApplication(sys.argv)
 
 windows = QMainWindow()
+central_widget = QWidget()
+windows.setCentralWidget(central_widget)
+
+layout = QFormLayout()
+central_widget.setLayout(layout)
 
 windows.setWindowTitle("Factor")
 windows.resize(900,600)
-label = QLabel("Factor Accounting System",windows)
-label.move(50,50)
 
-label_name = QLabel("کد محصول",windows)
-label_name.move(50,150)
-code_input = QLineEdit(windows)
-code_input.move(150,150)
-label_name = QLabel("نام محصول",windows)
-label_name.move(50,180)
-name_input = QLineEdit(windows)
-name_input.move(150,180)
-label_name = QLabel("قیمت محصول",windows)
-label_name.move(50,210)
-price_input = QLineEdit(windows)
-price_input.move(150,210)
-label_name = QLabel("مقدار محصول",windows)
-label_name.move(50,240)
-stock_input = QLineEdit(windows)
-stock_input.move(150,240)
-button = QPushButton("ثبت محصول", windows)
-button.move(150,270)
-input_user = QLineEdit(windows)
+code_input = QLineEdit()
+layout.addRow("کد محصول", code_input)
+name_input = QLineEdit()
+layout.addRow("نام محصول", name_input)
+price_input = QLineEdit()
+layout.addRow("قیمت محصول", price_input)
+stock_input = QLineEdit()
+layout.addRow("مقدار محصول", stock_input)
+button = QPushButton("ثبت محصول")
+layout.addRow(button)
 
-input_user.move(200,50)
 def push_button():
     if name_input.text() == "":
         print("is not none define")
@@ -43,4 +44,3 @@ def push_button():
 button.clicked.connect(push_button)    
 windows.show()
 sys.exit(app.exec())
-
